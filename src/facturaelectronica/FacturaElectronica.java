a5206f1aa06da8790173e3ba566e76258cdfc819
copoliyo <copoliyo@gmail.com>
2017-01-30 13:16:44 +0100

    Intentando firmar, esto es imposible.

--- src/facturaelectronica/FacturaElectronica.java
+++ src/facturaelectronica/FacturaElectronica.java
@@ -9,17 +9,49 @@ import es.facturae.facturae._2014.v3_2_1.facturae.*;
 import es.facturae.facturae._2014.v3_2_1.facturae.InvoiceType.TaxesOutputs;
 import org.w3._2000._09.xmldsig.*;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableEntryException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.crypto.MarshalException;
+import javax.xml.crypto.dsig.CanonicalizationMethod;
+import javax.xml.crypto.dsig.DigestMethod;
+import javax.xml.crypto.dsig.Reference;
+import javax.xml.crypto.dsig.SignatureMethod;
+import javax.xml.crypto.dsig.SignedInfo;
+import javax.xml.crypto.dsig.Transform;
+import javax.xml.crypto.dsig.XMLSignature;
+import javax.xml.crypto.dsig.XMLSignatureException;
+import javax.xml.crypto.dsig.XMLSignatureFactory;
+import javax.xml.crypto.dsig.dom.DOMSignContext;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+import javax.xml.crypto.dsig.keyinfo.KeyInfoFactory;
+import javax.xml.crypto.dsig.keyinfo.X509Data;
+import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
+import javax.xml.crypto.dsig.spec.TransformParameterSpec;
 import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 
 /**
@@ -31,7 +63,7 @@ public class FacturaElectronica {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, KeyStoreException, CertificateException, UnrecoverableEntryException, SAXException, MarshalException, XMLSignatureException {
         // TODO code application logic here
 
         FacturaDena fd = new FacturaDena();
@@ -39,7 +71,7 @@ public class FacturaElectronica {
             System.out.println("Factura Dena cargada.");
             try {
                 generaXmlFacturaE(fd);
-            } catch (JAXBException | IOException e) {
+            } catch (JAXBException | IOException | ParserConfigurationException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
@@ -47,7 +79,7 @@ public class FacturaElectronica {
 
     }
 
-    public static void generaXmlFacturaE(FacturaDena facturaDena) throws JAXBException, IOException {
+    public static void generaXmlFacturaE(FacturaDena facturaDena) throws JAXBException, IOException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, KeyStoreException, CertificateException, UnrecoverableEntryException, ParserConfigurationException, SAXException, MarshalException, XMLSignatureException {
         Facturae facturae = new Facturae();
 
         FileHeaderType fileHeader = new FileHeaderType();
@@ -272,24 +304,7 @@ public class FacturaElectronica {
 
         facturae.setInvoices(invoices);
         
-        
-        ///// A partir de aquí, la firma....
-        
-        SignatureType signature = new SignatureType();        
-        signature.setId("1234567890");
-        SignedInfoType signedInfo = new SignedInfoType();
-        signedInfo.setId("0987654321");
-        
-        CanonicalizationMethodType canonicalizationMethod = new CanonicalizationMethodType();
-        canonicalizationMethod.setAlgorithm("http://www.w3.org/TR/2001/REC-xml-c14n-20010315");
-        signedInfo.setCanonicalizationMethod(canonicalizationMethod);
-        
-        signature.setSignedInfo(signedInfo);
-        
-        facturae.setSignature(signature);
-        
-
-        File f = new File("new.xml");
+         File f = new File("new.xml");
 
         JAXBContext context = JAXBContext.newInstance(Facturae.class);
 
@@ -299,6 +314,73 @@ public class FacturaElectronica {
         //jaxbMarshaller.setProperty(Marshaller.JAXB_ENCODING, javax.xml.XMLConstants.DEFAULT_NS_PREFIX);
         jaxbMarshaller.marshal(facturae, f);
         jaxbMarshaller.marshal(facturae, System.out);
+        
+        
+        
+        
+        ///// A partir de aquí, la firma....
+        
+        // Create a DOM XMLSignatureFactory that will be used to
+        // generate the enveloped signature.
+        XMLSignatureFactory fac = XMLSignatureFactory.getInstance("DOM");       
+
+        // Create a Reference to the enveloped document (in this case,
+        // you are signing the whole document, so a URI of "" signifies
+        // that, and also specify the SHA1 digest algorithm and
+        // the ENVELOPED Transform.
+        Reference ref = fac.newReference("", fac.newDigestMethod(DigestMethod.SHA1, null),
+                Collections.singletonList(fac.newTransform(Transform.ENVELOPED, (TransformParameterSpec) null)),
+                null, null);
+        
+        // Create the SignedInfo.
+        SignedInfo si = fac.newSignedInfo(fac.newCanonicalizationMethod(CanonicalizationMethod.INCLUSIVE,
+                (C14NMethodParameterSpec) null),
+                fac.newSignatureMethod(SignatureMethod.RSA_SHA1, null),
+                Collections.singletonList(ref));                     
+        
+       
+       
+        // Load the KeyStore and get the signing key and certificate.
+        KeyStore ks = KeyStore.getInstance("JKS");
+        ks.load(new FileInputStream("C:\\Users\\Txus\\.keystore"), "copoliyo".toCharArray());
+        
+        //KeyStore.PrivateKeyEntry keyEntry
+        //        = (KeyStore.PrivateKeyEntry) ks.getEntry("mykey", new KeyStore.PasswordProtection("copoliyo".toCharArray()));
+        
+        KeyStore.TrustedCertificateEntry keyEntry = (KeyStore.TrustedCertificateEntry) ks.getEntry("mykey", null);
+        //X509Certificate cert = (X509Certificate) keyEntry.getCertificate();
+        
+        X509Certificate cert = (X509Certificate) keyEntry.getTrustedCertificate();
+
+        // Create the KeyInfo containing the X509Data.
+        KeyInfoFactory kif = fac.getKeyInfoFactory();
+        List x509Content = new ArrayList();
+        x509Content.add(cert.getSubjectX500Principal().getName());
+        x509Content.add(cert);
+        X509Data xd = kif.newX509Data(x509Content);
+        KeyInfo ki = kif.newKeyInfo(Collections.singletonList(xd));
+        
+        
+        // Instantiate the document to be signed.
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        Document doc = dbf.newDocumentBuilder().parse(new FileInputStream("new.xml"));
+
+        // Create a DOMSignContext and specify the RSA PrivateKey and
+        // location of the resulting XMLSignature's parent element.
+        DOMSignContext dsc = new DOMSignContext((Key) keyEntry.getTrustedCertificate(), doc.getDocumentElement());
+        
+        // Create the XMLSignature, but don't sign it yet.
+        XMLSignature signature = fac.newXMLSignature(si, ki);
+
+        // Marshal, generate, and sign the enveloped signature.
+        signature.sign(dsc);
+        
+        
+       
+        
+
+       
 
     }
 
